a772aa5e0d16144ca96c673d10667689511fa1d4
Hervegmz <107627783+Hervegmz@users.noreply.github.com>
2023-11-13 11:48:13 +0000

    new excel_to_rdf

--- excel_to_rdf.py
+++ excel_to_rdf.py
@@ -1,29 +1,32 @@
 import rdflib
 import pandas as pd
+
 '''Python file use to convert data from excel to turtle format'''
+
 g = rdflib.ConjunctiveGraph()
 
 skos = rdflib.Namespace("http://www.w3.org/2004/02/skos/core#")
 rdf = rdflib.Namespace("http://www.w3.org/1999/02/22-rdf-syntax-ns#")
 owl = rdflib.Namespace("http://www.w3.org/2002/07/owl#")
 rdfs = rdflib.Namespace("http://www.w3.org/2000/01/rdf-schema#")
-aia = rdflib.Namespace("aia:")
-gdpr = rdflib.Namespace("gdpr:")
 
-df = pd.read_excel("post_knowledge_graph.xlsx")
+df = pd.read_excel(".\intermediate_excel_files\competency_questions_evaluated.xlsx",sheet_name="NO_answers")
 
 for index, row in df.iterrows():
-    #name = row['Question']
-    name="Data Sharing"
+    name = row['Label']
     name = name.replace(" ","_")
-    question = rdflib.URIRef(name)
-    #aia_ID = aia[name]
-    #first_descriptors = row['Descriptors'].split(',')[0].strip(" []'\" ")
-    unique_id = f"{row['Original Statement']}"
-    gdpr_point = rdflib.URIRef(unique_id)
-    heading = row['Label']
-    g.add((question, rdf.type, skos.Concept,rdflib.Literal(question,lang="en")))
-    g.add((question, rdfs.prefLabel, rdflib.Literal(heading, lang="en")))
-    g.add((question, skos.definition, rdflib.Literal(gdpr_point, lang="en")))
+    label = rdflib.URIRef(name)
+    question_node = rdflib.URIRef(row['Question'].replace(" ", "_"))
 
-g.serialize("RDF_questions.ttl",format="turtle")
+    g.add((question_node, rdf.type, rdf.Property, rdflib.Literal(row['Question'], lang="en")))
+
+    g.add((question_node, skos.definition, rdflib.Literal(f"{row['Original Statement']}", lang="en")))
+
+    g.add((question_node, rdf.explanation, rdflib.Literal(row['Explanation'], lang="en")))
+
+    g.add((label, rdf.type, skos.Concept))
+    g.add((label, rdf.question, question_node))
+
+# Serialize the graph to a Turtle file
+g.serialize(".\extra\RDF_extra.ttl", format="turtle")
+
